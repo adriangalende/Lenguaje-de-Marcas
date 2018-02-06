8fefc3a6c57ae81ba68fddbcf292be3303b37bba
Adrián <adriangalende@gmail.com>
2018-02-06 11:58:30 +0100

    El orden altera al producto

--- Examen/js/mundoPerdido.js
+++ Examen/js/mundoPerdido.js
@@ -2,13 +2,15 @@ window.onload = function() {
     document.getElementById("mainImage").addEventListener("click", function(e){
         var parrafos = document.getElementsByTagName("p");
         
-    var para = document.createElement("P");                       
-    var t = document.createTextNode("");       
+		var para = document.createElement("P");                       
+		var t = document.createTextNode("");   
+		
+		para.append("Antes habia " + parrafos.length + 
+		" parrafos, ahora hay " + (parrafos.length +1 ) + "");
+		
+		parrafos[parrafos.length-2].appendChild(para); 
 
-    parrafos[parrafos.length-2].appendChild(para); 
 
-    parrafos[parrafos.length-2].append("Antes habia " + parrafos.length + 
-    " parrafos, ahora hay " + (parrafos.length+1) + "");
     
     });
   };
